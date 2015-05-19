bec360c6c8b0c3c53218fced2111715b4c81896d
Matthew Sykes <matthew.sykes@gmail.com>
2015-05-19 11:54:29 -0400

    Ensure SSH does not break implicit health check
    [#94455686]

--- diego/ssh_test.go
+++ diego/ssh_test.go
@@ -22,17 +22,20 @@ var _ = Describe("SSH", func() {
 
 	BeforeEach(func() {
 		appName = generator.RandomName()
-		Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Dora, "--no-start", "-b", "ruby_buildpack", "-i", "2"), CF_PUSH_TIMEOUT).Should(Exit(0))
 	})
 
 	AfterEach(func() {
-		Eventually(cf.Cf("logs", appName, "--recent")).Should(Exit())
 		Eventually(cf.Cf("delete", appName, "-f")).Should(Exit(0))
 	})
 
 	enableSSH := func(appName string) {
 		guid := guidForAppName(appName)
 		Eventually(cf.Cf("curl", "/v2/apps/"+guid, "-X", "PUT", "-d", `{"diego": true,"enable_ssh": true}`)).Should(Exit(0))
+	}
+
+	disableHealthCheck := func(appName string) {
+		guid := guidForAppName(appName)
+		Eventually(cf.Cf("curl", "/v2/apps/"+guid, "-X", "PUT", "-d", `{"health_check_type": "none"}`)).Should(Exit(0))
 	}
 
 	oauthToken := func() string {
@@ -62,8 +65,10 @@ var _ = Describe("SSH", func() {
 		return response.AppSSHEndpoint
 	}
 
-	Describe("An App running on Diego with enable_ssh on", func() {
+	Describe("An App running on Diego with ssh enabled", func() {
 		BeforeEach(func() {
+			Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Dora, "--no-start", "-b", "ruby_buildpack", "-i", "2"), CF_PUSH_TIMEOUT).Should(Exit(0))
+
 			enableSSH(appName)
 
 			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
@@ -104,6 +109,39 @@ var _ = Describe("SSH", func() {
 			Expect(err).To(HaveOccurred())
 
 			Eventually(cf.Cf("events", appName)).Should(Say("audit.app.ssh-unauthorized"))
+		})
+	})
+
+	Describe("An app with ssh enabled an implicit process check", func() {
+		BeforeEach(func() {
+			startCommand := "echo starting; sleep 15; echo done; exit 0"
+			Eventually(cf.Cf("push", appName, "-p", assets.NewAssets().Standalone, "--no-route", "--no-start", "-b", ZIP_NULL_BUILDPACK, "-c", startCommand), CF_PUSH_TIMEOUT).Should(Exit(0))
+
+			enableSSH(appName)
+			disableHealthCheck(appName)
+
+			Eventually(cf.Cf("start", appName), CF_PUSH_TIMEOUT).Should(Exit(0))
+		})
+
+		It("is detected as crashed when it termintes even when the daemon is running", func() {
+			password := oauthToken()
+
+			clientConfig := &ssh.ClientConfig{
+				User: fmt.Sprintf("cf:%s/%d", guidForAppName(appName), 0),
+				Auth: []ssh.AuthMethod{ssh.Password(password)},
+			}
+
+			client, err := ssh.Dial("tcp", sshProxyAddress(), clientConfig)
+			Expect(err).NotTo(HaveOccurred())
+
+			session, err := client.NewSession()
+			Expect(err).NotTo(HaveOccurred())
+
+			err = session.Run("true")
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(cf.Cf("logs", appName, "--recent")).Should(Say(`"reason"=>"CRASHED"`))
+			Eventually(cf.Cf("events", appName)).Should(Say(fmt.Sprintf("app.crash.*%s", appName)))
 		})
 	})
 })
